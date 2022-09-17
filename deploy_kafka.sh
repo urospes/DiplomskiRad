47025ae49f0e1e8d8ce020d1a506c14d0f79ed38
unknown <pesic.uros@hotmail.com>
2022-09-17 02:22:08 +0200

    kafka deployment

---
+++ deploy_kafka.sh
@@ -0,0 +1,5 @@
+kubectl create namespace kafka
+kubectl create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
+kubectl apply -f https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml -n kafka
+kubectl wait kafka/my-cluster --for=condition=Ready --timeout=300s -n kafka
+read -p "Press any key to exit..." x
--- kubernetes_deployments.sh
+++ kubernetes_deployments.sh
@@ -1,6 +1,7 @@
 cd "$(dirname "$0")"
-cd ./K8s
+cd ./K8s/ConfigMaps
 kubectl apply -f mosquitto_configmap.yaml
+cd ../Deployments
 kubectl apply -f mosquitto_deployment.yaml
 kubectl apply -f analytics_deployment.yaml
 read -p "Press any key to exit..." x
